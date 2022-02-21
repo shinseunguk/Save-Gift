af24b167adb7cb89c5968a7103767eb87c232222
SeungUkShin <krdut1@gmail.com>
2022-02-21 23:05:22 +0900

    chore: imageview margin 작업중...

--- Save Gift/CollectionViewCell.swift
+++ Save Gift/CollectionViewCell.swift
@@ -10,10 +10,13 @@ import UIKit
 class CollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var collectionLabel: UILabel!
+    @IBOutlet weak var cellImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        print("CollectionViewCell awakeFromNib")
+        collectionLabel.text = "gdgd"
     }
 
 }
