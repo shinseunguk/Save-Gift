fe6c0f1a50212b0ec5627dfc5c506eb222e271a9
SeungUkShin <krdut1@gmail.com>
2022-03-10 17:55:16 +0900

    chore: customCell 작업중..

---
+++ Save Gift/GetFriendTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  GetFriendTableViewCell.swift
+//  Save Gift
+//
+//  Created by mac on 2022/03/10.
+//
+
+import UIKit
+
+class GetFriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
