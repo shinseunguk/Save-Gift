a4d2df98b05b9a48ca55be44361ba28113db77f9
SeungUkShin <krdut1@gmail.com>
2022-03-17 16:02:11 +0900

    chore: 유효기간 formatting

--- Save Gift/UITextFiled+Extension.swift
+++ Save Gift/UITextFiled+Extension.swift
@@ -82,6 +82,19 @@ extension String {
         }
     }
     
+    func substring(from: Int, to: Int) -> String {
+            guard from < count, to >= 0, to - from >= 0 else {
+                return ""
+            }
+            
+            // Index 값 획득
+            let startIndex = index(self.startIndex, offsetBy: from)
+            let endIndex = index(self.startIndex, offsetBy: to + 1) // '+1'이 있는 이유: endIndex는 문자열의 마지막 그 다음을 가리키기 때문
+            
+            // 파싱
+            return String(self[startIndex ..< endIndex])
+        }
+    
     func getLocalURL() -> String{
         return "http://175.212.211.98:8008"
 //        return "http://172.30.1.50:8008"
