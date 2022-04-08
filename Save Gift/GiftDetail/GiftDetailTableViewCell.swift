c71fcc771cb8bc9d794749f52f3e087a03a28464
SeungUkShin <krdut1@gmail.com>
2022-04-08 17:48:02 +0900

    [신승욱] 기프티콘 상세페이지 작업중

---
+++ Save Gift/GiftDetail/GiftDetailTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  GiftDetailTableViewCell.swift
+//  Save Gift
+//
+//  Created by mac on 2022/04/08.
+//
+
+import UIKit
+
+class GiftDetailTableViewCell: UITableViewCell {
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var copyBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func copyAction(_ sender: Any) {
+        UIPasteboard.general.string = secondLabel.text!
+        print("secondLabel", secondLabel.text!)
+    }
+}
