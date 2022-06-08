adae208860ded8502a8a05209a091f93b1e56c91
Wetax_iOS <wetaxmobile@gmail.com>
2022-06-08 17:46:23 +0900

    [신승욱] 선물함 내가 보낸 선물 작업중

---
+++ Save Gift/GiftDetail/PresentMessageViewTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PresentMessageViewTableViewCell.swift
+//  Save Gift
+//
+//  Created by mac on 2022/06/08.
+//
+
+import UIKit
+
+class PresentMessageViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var upLabel: UILabel!
+    @IBOutlet weak var downLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
