f0488bd5636b78330db9aea8e84de78c5b3c294d
Wetax_iOS <wetaxmobile@gmail.com>
2022-06-27 17:23:34 +0900

    [신승욱] FAQ 상세화면 init

---
+++ Save Gift/Setting/SettingHowToUse2Controller.swift
@@ -0,0 +1,15 @@
+//
+//  SettingHowToUse2Controller.swift
+//  Save Gift
+//
+//  Created by mac on 2022/06/27.
+//
+
+import Foundation
+import UIKit
+
+class SettingHowToUse2Controller : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
