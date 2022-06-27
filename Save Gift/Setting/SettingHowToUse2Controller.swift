37ba12de4bd60cf755a7b77dc5d4cb0c1f6bd9ae
SeungUkShin <krdut1@gmail.com>
2022-06-27 23:56:14 +0900

    [신승욱] FAQ 작업중

--- Save Gift/Setting/SettingHowToUse2Controller.swift
+++ Save Gift/Setting/SettingHowToUse2Controller.swift
@@ -9,7 +9,18 @@ import Foundation
 import UIKit
 
 class SettingHowToUse2Controller : UIViewController {
+    
+    var aTitle : String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.tintColor = .systemBlue
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(displayP3Red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
+        self.navigationItem.title = "\(aTitle!)"
+        
     }
 }
